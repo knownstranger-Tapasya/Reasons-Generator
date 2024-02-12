d8d9bb9544b229b50ab05c22adf2d804133d27e7
TAPASYA BAIDYA <83876783+knownstranger-Tapasya@users.noreply.github.com>
2024-02-12 17:07:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,361 @@
+let reason = [{
+        reason: "To conquer your fears. ",
+    },
+    {
+        reason: "To see your family again. ",
+    },
+    {
+        reason: "To see your favorite artist live. ",
+    },
+    {
+        reason: "To listen to music again. ",
+    },
+    {
+        reason: "To experience a new culture.",
+    },
+    {
+        reason: "To make new friends.",
+    },
+    {
+        reason: "To inspire.",
+    },
+    {
+        reason: "To have your own children.",
+    },
+    {
+        reason: "To adopt your own pet.",
+    },
+    {
+        reason: "To make yourself proud.",
+    },
+    {
+        reason: "To meet your idols.",
+    },
+    {
+        reason: "To laugh until you cry.",
+    },
+    {
+        reason: "To feel tears of happiness.",
+    },
+    {
+        reason: "To eat your favorite food.",
+    },
+    {
+        reason: "To see your siblings grow.",
+    },
+    {
+        reason: "To pass school.",
+    },
+    {
+        reason: "To get a tattoo.",
+    },
+    {
+        reason: "To smile until your cheeks hurt.",
+    },
+    {
+        reason: "To meet your internet friends.",
+    },
+    {
+        reason: "To find someone who loves you like you deserve.",
+    },
+    {
+
+        reason: "To eat ice cream on a hot day.",
+    },
+    {
+        reason: "To drink hot chocolate on a cold day.",
+    },
+    {
+        reason: "To see untouched snow in the morning.",
+    },
+    {
+        reason: "To see a sunset that sets the sky on fire.",
+    },
+    {
+        reason: "To see stars light up the sky.",
+    },
+    {
+        reason: "To read a book that changes your life.",
+    },
+    {
+        reason: "To see the flowers in the spring.",
+    },
+    {
+        reason: "To see the leaves change from green to brown.",
+    },
+    {
+        reason: "To travel abroad.",
+    },
+    {
+        reason: "To learn a new language.",
+    },
+    {
+        reason: "To learn a new language.",
+    },
+    {
+        reason: "To learn to draw. ",
+    },
+    {
+        reason: "To tell others your story in the hopes of helping them.",
+    },
+    {
+        reason: "To get Puppy kisses.",
+    },
+    {
+        reason: "To get Baby kisses (the open mouthed kind when they smack their lips on your cheek).        ",
+    },
+    {
+        reason: "To swear words and the release you feel when you say them.        .",
+    },
+    {
+        reason: "Trampolines.",
+    },
+    {
+        reason: "Food.",
+    },
+    {
+        reason: "Stargazing.",
+    },
+    {
+        reason: "Cloud watching.",
+    },
+    {
+        reason: "Taking a shower and then sleeping in clean sheets.",
+    },
+    {
+        reason: "Receiving thoughtful gifts.",
+    },
+    {
+        reason: 'To hear "I saw this and thought of you." ',
+    },
+    {
+        reason: 'The feeling you get when someone you love says, "I love you."        ',
+    },
+    {
+        reason: "The relief you feel after crying.",
+    },
+    {
+        reason: "Sunshine.",
+    },
+    {
+        reason: "The feeling you get when someone is listening to you/giving you their full attention. 48. Your future wedding.        ",
+    },
+    {
+        reason: "Your favorite candy bar.",
+    },
+    {
+        reason: "New clothes.",
+    },
+    {
+        reason: "Witty puns.",
+    },
+    {
+        reason: "Really good bread.",
+    },
+    {
+        reason: "Holding your child in your arms for the first time.",
+    },
+    {
+        reason: "Completing a milestone (aka going to college,graduating college, getting married, getting your dream job.)",
+    },
+    {
+        reason: "The kind of dreams where you wake up and can't stop smiling.",
+    },
+    {
+        reason: "The smell before and after it rains.",
+    },
+    {
+        reason: "The sound of rain against a rooftop.",
+    },
+    {
+        reason: "The feeling you get when you're dancing.",
+    },
+    {
+        reason: "Experience a storm.",
+    },
+    {
+        reason: "Trying out new recipe.",
+    },
+    {
+        reason: "The feeling you get when your favorite song comes on the radio.",
+    },
+    {
+        reason: "The rush you get when you step onto a stage.",
+    },
+    {
+        reason: "You have to share your voice and talents and knowledge with the world because they are so valuable.",
+    },
+    {
+        reason: "Breakfast in bed.",
+    },
+    {
+        reason: "Getting a middle seat in the movie theater.",
+    },
+    {
+        reason: "Breakfast for dinner (because it's so much better at night than in the morning).",
+    },
+    {
+        reason: "Sitting on a rooftop with the love of your life.",
+    },
+    {
+        reason: "Forgiveness.",
+    },
+    {
+        reason: "Water balloon fights",
+    },
+    {
+        reason: "New books by your favorite authors.",
+    },
+    {
+        reason: "Fireflies.",
+    },
+    {
+        reason: "Birthdays.",
+    },
+    {
+        reason: "Realizing that someone loves you.",
+    },
+    {
+        reason: "Spending the day with someone you love.",
+    },
+    {
+        reason: "Spending the whole day in bed.",
+    },
+    {
+        reason: "Eating a whole pint of your favorite ice cream.",
+    },
+    {
+        reason: " Floating in water on your back and just staring up at the sky.",
+    },
+    {
+        reason: "First dates (even the bad ones make for funny stories).",
+    },
+    {
+        reason: "Bonfires and s'mores.",
+    },
+    {
+        reason: "Relationships where you love someone but aren't in love with them.",
+    },
+    {
+        reason: "Coming home to someone you love.",
+    },
+    {
+        reason: "The color of autumn leaves when they change.",
+    },
+    {
+        reason: "Singing songs at the top of your lungs with your friends.",
+    },
+    {
+        reason: "Cuddling.",
+    },
+    {
+        reason: "Being wrapped up in a warm bed.",
+    },
+    {
+        reason: "Someone's skin against yours.",
+    },
+    {
+        reason: "Holding hands.",
+    },
+    {
+        reason: "The kind of hugs when you can feel a weight being lifted off your shoulders. The kind of hug where your breath syncs with the other person's, and you feel like the only two people in the world.",
+    },
+    {
+        reason: "Singing off key with your best friends.",
+    },
+    {
+        reason: "Road trips.",
+    },
+    {
+        reason: "Spontaneous adventures.",
+    },
+    {
+        reason: "The feeling of sand beneath your toes.",
+    },
+    {
+        reason: "The feeling when the first ocean wave rolls up and envelops your toes and ankles and knees.",
+    },
+    {
+        reason: "Hugs and Kisses.",
+    },
+    {
+        reason: "To learn an instrument.",
+    },
+    {
+        reason: "Your first (or hundredth) trip to Disneyland.",
+    },
+    {
+        reason: "Yourn first salary.",
+    },
+    {
+        reason: "The taste of your favorite food.",
+    },
+    {
+        reason: "The child-like feeling you get on Christmas morning.",
+    },
+    {
+        reason: "The day when everything finally goes your way.",
+    },
+    {
+        reason: "Compliments and praise.",
+    },
+    {
+        reason: "To look.",
+    }
+];
+
+//Function to randomly select a reason value and return a random reason object from the reasons array
+function getRandomReason() {
+    let randomNumber = Math.floor(Math.random() * (reason.length));
+    let randomReason = reason[randomNumber];
+    return randomReason;
+}
+// Function to randomly select a reason value and return a random reason object from the reasons array
+function getRandomReason() {
+    let randomNumber = Math.floor(Math.random() * reason.length);
+    return reason[randomNumber];
+}
+
+// Function to select random gradient background
+function getRandomGradient() {
+    let startRed = Math.floor(Math.random() * 256);
+    let startGreen = Math.floor(Math.random() * 256);
+    let startBlue = Math.floor(Math.random() * 256);
+
+    let endRed = Math.floor(Math.random() * 256);
+    let endGreen = Math.floor(Math.random() * 256);
+    let endBlue = Math.floor(Math.random() * 256);
+
+    let randomGradient = `radial-gradient(circle, rgb(${startRed}, ${startGreen}, ${startBlue}), rgb(${endRed}, ${endGreen}, ${endBlue}))`;
+
+    return randomGradient;
+}
+
+
+//Function to call the getRandomReason function and stores the returned reason object in a variable
+//Constructs a string containing the different properties of the reason object 
+function printReason() {
+    let reason = getRandomReason();
+    let reasonContainer = document.getElementById("reason-box");
+    let reasonString = `<p class="reason">${reason.reason}</p>`;
+    if (reason.citation) { reasonString += `<span class="citation">${reason.citation}</span>` }
+    if (reason.year) { reasonString += `<span class="year">${reason.year}</span></p>` } else { reasonString += '</p>' };
+    reasonContainer.innerHTML = reasonString;
+
+    //assigns random color value to document background color
+    document.body.style.background = getRandomGradient();
+
+}
+
+//reason automatically refreshes every 15 seconds
+window.setInterval(function() {
+    printReason();
+}, 15000);
+
+//Event listener on LoadReason button to generate new reason
+document.getElementById("loadReason").addEventListener("click", printReason, false);
+
+// Call printReason() once the window is fully loaded
+window.addEventListener('load', function() {
+    printReason();
+});
